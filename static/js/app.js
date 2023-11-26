bc91b436d85b2dfcd7c1f5bf0315f583c92dd940
Jorge Nardy <nardyjh@gmail.com>
2023-11-26 18:39:12 -0500

    main challenge complete

--- static/js/app.js
+++ static/js/app.js
@@ -1,52 +1,97 @@
-// Use D3 to fetch the data from the JSON file
+// Use D3 to fetch data from JSON
 const samplesURL = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-d3.json(samplesURL).then(function(data) {
-  console.log(data);
+// Function to update all plots based on demos selected 
+function optionChanged(selectedSample) {
+  d3.json(samplesURL).then(function(data) {
+    console.log(data);
 
-  // Horizontal bar chart with a dropdown menu to display the top 10 OTUs
+    // Create arrays for OTU values that updates
+    let sampleValues = data.samples.find(sample => sample.id === selectedSample).sample_values;
+    let otuIds = data.samples.find(sample => sample.id === selectedSample).otu_ids;
+    let otuLabels = data.samples.find(sample => sample.id === selectedSample).otu_labels;
 
-  // Creating arrays for OTU values 
-  let sample_values = data.samples.map(sample => sample.sample_values);
-  let otu_ids = data.samples.map(sample => sample.otu_ids);
-  let otu_labels = data.samples.map(sample => sample.otu_labels);
+    // Horizontal bar chart 
+    // Sort data in descending order
+    let indices = sampleValues.map((_, i) => i);
+    indices.sort((a, b) => sampleValues[b] - sampleValues[a]);
 
-  // Sort data in descending order
-  let indices = sample_values[0].map((_, i) => i); 
-  indices.sort((a, b) => sample_values[0][b] - sample_values[0][a]);
+    // Trace data while slicing and reversing for Plotly's defaults 
+    let trace1 = {
+      x: sampleValues.filter((value, i) => indices.includes(i)).slice(0, 10).reverse(),
+      y: otuIds.filter((value, i) => indices.includes(i)).map(id => `OTU ${id}`).slice(0, 10).reverse(),
+      text: otuLabels.filter((value, i) => indices.includes(i)).slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h"
+    };
 
-  // Slice the first 10 objects for plotting
-  let slicedIndices = indices.slice(0, 10);
+    // plotData array
+    let plotData = [trace1];
 
-  // Reverse array for Plotly's defaults 
-  slicedIndices.reverse();
-
-  // Trace data
-  let trace1 = {
-    x: sample_values[0].filter((value, i) => slicedIndices.includes(i)),
-    y: otu_ids[0].filter((value, i) => slicedIndices.includes(i)).map(id => `OTU ${id}`),
-    text: otu_labels[0].filter((value, i) => slicedIndices.includes(i)),
-    type: "bar",
-    orientation: "h"
-  };
-
-  // plotData array
-  let plotData = [trace1];
-
-  // Apply a title to the layout
-  let layout = {
-    title: "Top 10 OTUs per Individual",
-    margin: {
+    // Apply a title to the layout
+    let layout = {
+      title: "Top 10 OTUs per Individual",
+      margin: {
         l: 100,
         r: 100,
         t: 100,
         b: 100
       }
-  };
+    };
 
-  // Render the plot to the div tag with id "plot"
-  Plotly.newPlot("bar", plotData, layout);
+    // Render the plot to the div tag with id "bar"
+    Plotly.newPlot("bar", plotData, layout);
 
+    // Bubble chart
+    // Trace data 
+    let trace2 = {
+      x: data.samples.find(sample => sample.id === selectedSample).otu_ids,
+      y: data.samples.find(sample => sample.id === selectedSample).sample_values,
+      mode: 'markers',
+      marker: {
+        size: data.samples.find(sample => sample.id === selectedSample).sample_values,
+        color: data.samples.find(sample => sample.id === selectedSample).otu_ids,
+        colorscale: 'Viridis'
+      },
+      text: data.samples.find(sample => sample.id === selectedSample).otu_labels
+    };
+
+    // plotData array for bubble chart
+    let plotDataBubble = [trace2];
+
+    // Apply title to the layout
+    let layoutBubble = {
+      title: 'Bubble Chart for Each Sample',
+      xaxis: { title: 'OTU IDs' },
+      yaxis: { title: 'Sample Values' },
+      showlegend: false
+    };
+
+    // Render the plot to the div tag with id "bubble"
+    Plotly.newPlot('bubble', plotDataBubble, layoutBubble);
+
+    // Display sample metadata
+    let metadata = data.metadata.find(entry => entry.id == selectedSample);
+    let demographicInfo = "";
+    Object.entries(metadata).forEach(([key, value]) => {
+      demographicInfo += `<p><strong>${key}:</strong> ${value}</p>`;
+    });
+    document.getElementById('sample-metadata').innerHTML = demographicInfo;
+
+  }).catch(function(error) {
+    console.log(error);
+  });
+}
+
+// Initialize dropdown options when the page loads
+d3.json(samplesURL).then(function(data) {
+  let dropdown = d3.select("#selDataset");
+  data.names.forEach(function(name) {
+    dropdown.append("option").text(name).property("value", name);
+  });
+
+  // Call the optionChanged function with the first sample to initialize the page
+  optionChanged(data.names[0]);
 }).catch(function(error) {
   console.log(error);
 });
